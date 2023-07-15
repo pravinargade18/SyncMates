b2b6035f0fda0ecc13a57ecbeb515a0972ba187d
pravinargade18 <pravinargade18@gmail.com>
2023-07-15 18:58:38 +0530

    feat: Add and remove profile picture

--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -12,8 +12,9 @@ import {profileColors}  from '../utils/constants'
 import { toast } from "react-toastify";
 import ToastMessage from "../components/ToastMessage";
 import { doc, updateDoc } from 'firebase/firestore';
-import { auth, db } from '../firebase/firebase.config';
+import { auth, db, storage } from '../firebase/firebase.config';
 import { updateProfile } from 'firebase/auth';
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
 
 
 
@@ -25,6 +26,55 @@ const LeftNav = () => {
     const [nameEdited,setNameEdited] = useState(false);
 
     const {currentUser,signOut,setCurrentUser}=useAuth();
+
+
+    //image upload to firestore
+
+     const uploadImageToFirestore =(file)=>{
+        try {
+            if(file){
+              // file uploading logic  --> firebase docs -->storage ->upload files
+                const storageRef = ref(storage, `images/${currentUser.displayName}`);  //basically filename has to be given so we gave username
+
+                const uploadTask = uploadBytesResumable(storageRef, file);
+
+                uploadTask.on(
+                  "state_changed",
+                  (snapshot) => {
+                    const progress =
+                      (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log("Upload is " + progress + "% done");
+                    switch (snapshot.state) {
+                      case "paused":
+                        console.log("Upload is paused");
+                        break;
+                      case "running":
+                        console.log("Upload is running");
+                        break;
+                    }
+                  },
+                  (error) => {
+                    console.log(error);
+                  },
+                  () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then(
+                      async (downloadURL) => {  //async added as updateprofile is used
+                        console.log("File available at", downloadURL);
+                       
+                        handleUpdateProfile('photo',downloadURL);
+                         await updateProfile(auth.currentUser, {
+                           photoURL: downloadURL,
+                          });
+
+                      }
+                    );
+                  }
+                );
+            }
+        } catch (error) {
+          console.log(error);
+        }
+     }
 
     const handleUpdateProfile = (type,value)=>{
         //types-->color, photo ,remove photo ,change name
@@ -135,11 +185,13 @@ const LeftNav = () => {
                 id="fileUpload"
                 type="file"
                 style={{ display: "none" }}
-                onChange={() => {}}
+                onChange={(e) => 
+                    uploadImageToFirestore(e.target.files[0])
+                }
               />
             </div>
             {currentUser?.photoURL && (
-              <div className="w-6 h-6 rounded-full bg-red-500 flex justify-center items-center absolute right-0 bottom-0">
+              <div onClick={()=>handleUpdateProfile('photo-remove')} className="w-6 h-6 rounded-full bg-red-500 flex justify-center items-center absolute right-0 bottom-0">
                 <MdDeleteForever size={14} />
               </div>
             )}
