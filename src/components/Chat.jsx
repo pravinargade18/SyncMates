2a5447a4f6022f4829a94a7cbb66741a89994b81
pravinargade18 <pravinargade18@gmail.com>
2023-07-20 17:15:21 +0530

    feat: Add block user functionality

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,19 +1,39 @@
+import { useAuth } from "../context/authContext";
 import { useChatContext } from "../context/chatContext";
 import ChatFooter from "./ChatFooter";
 import ChatHeader from "./ChatHeader";
 import Messages from "./Messages";
 
 const Chat = () => {
-    const {data}=useChatContext();
 
+    const { currentUser } = useAuth();
+    const { data, users } = useChatContext();
+
+    
+
+    const isUserBlocked = users[currentUser.uid].blockedUsers?.find(
+      (u) => u === data.user.uid
+    ); //in my blockedUsers do I have any users blocked
+
+    const iAmBlocked = users[data.user.uid].blockedUsers?.find(
+      (u) => u === currentUser.uid //in other user does it consists my id in blockedUsers
+    );
 
   return (
     <div className="flex flex-col p-5 grow">
-        <ChatHeader/>
-        {data.chatId && <Messages/>}
-        <ChatFooter/>
+      <ChatHeader />
+      {data.chatId && <Messages />}
+      {!isUserBlocked && !iAmBlocked && <ChatFooter />}
+      {isUserBlocked && (
+        <div className="w-full text-center text-c3 py-5 text-red-500">
+          This user has been blocked!
+        </div>
+      )}
+      {iAmBlocked && (
+        <div className="w-full text-center text-c3 py-5 text-red-500">{`${data.user.displayName} has blocked you! `}</div>
+      )}
     </div>
-  )
+  );
 }
 
 export default Chat;
--- src/components/ChatMenu.jsx
+++ src/components/ChatMenu.jsx
@@ -1,25 +1,71 @@
-import React from 'react'
-import ClickAwayListener from 'react-click-away-listener';
+import { arrayRemove, arrayUnion, doc, updateDoc } from "firebase/firestore";
+import ClickAwayListener from "react-click-away-listener";
+import { db } from "../firebase/firebase.config";
+import { useAuth } from "../context/authContext";
+import { useChatContext } from "../context/chatContext";
 
-const ChatMenu = ({showMenu,setShowMenu}) => {
+const ChatMenu = ({ showMenu, setShowMenu }) => {
+  
+   const { currentUser } = useAuth();
+   const { data,users } = useChatContext();
 
-    const closeMenuHandler=()=>{
-            setShowMenu(false);
+   
+    const isUserBlocked = users[currentUser.uid].blockedUsers?.find(
+      (u) => u === data.user.uid
+    ); //in my blockedUsers do I have any users blocked
+
+    const iAmBlocked = users[data.user.uid].blockedUsers?.find(
+      (u) => u === currentUser.uid //in other user does it consists my id in blockedUsers
+    );
+
+         const closeMenuHandler = () => {
+           setShowMenu(false);
+         };
+
+
+  const blockUserHandler = async (action) => {
+    if (action === "block") {
+      await updateDoc(doc(db, "users", currentUser.uid), {
+        blockedUsers: arrayUnion(data.user.uid),
+      });
     }
+
+    if (action === "unblock") {
+      await updateDoc(doc(db, "users", currentUser.uid), {
+        blockedUsers: arrayRemove(data.user.uid),
+      });
+    }
+  };
+
+  
+
+
   return (
     <ClickAwayListener onClickAway={closeMenuHandler}>
       <div className="w-[200px] absolute top-[70px] right-5 bg-c0 z-10 rounded-md overflow-hidden">
         <ul className="flex flex-col py-2">
-          <li className="flex items-center py-3 px-5 hover:bg-black cursor-pointer">
-            Block user
-          </li>
-          <li className="flex items-center py-3 px-5 hover:bg-black cursor-pointer">
+          {!iAmBlocked && (
+            <li
+              onClick={(e) => {
+                e.stopPropagation();
+                blockUserHandler(isUserBlocked ? "unblock" : "block");
+              }}
+              className="flex items-center py-3 px-5 hover:bg-black cursor-pointer"
+            >
+              {isUserBlocked? 'Unblock user' : 'Block user'}
+            </li>
+          )}
+          <li onClick={(e)=>{ 
+            e.stopPropagation();
+            handleDelete();
+            setShowMenu(false);
+            }} className="flex items-center py-3 px-5 hover:bg-black cursor-pointer">
             Delete chat
           </li>
         </ul>
       </div>
     </ClickAwayListener>
   );
-}
+};
 
 export default ChatMenu;
